75dee1a2a856520619cbbda44e870d426ab1b163
Soyeb <tssoyeb@gmail.com>
2022-04-22 12:11:59 +0600

    update form on going

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import AddUser from './component/AddUser/AddUser';
 import Header from './component/Header/Header';
 import Home from './component/Home/Home';
+import UpdateUser from './component/UpdateUser/UpdateUser';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
   <Header></Header>
   <Routes>
 <Route path='/user'element={<Home></Home>}></Route>
+    <Route path='/updateUser' element={<UpdateUser></UpdateUser>}></Route>
     <Route path='/user/add' element={<AddUser></AddUser>}></Route>
   </Routes>
     </div>
--- src/component/UpdateUser/UpdateUser.js
+++ src/component/UpdateUser/UpdateUser.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
 const UpdateUser = () => {
+    const handleUpadteUser=(event)=>{
+        event.preventDefault()
+        const name =event.target.name.value
+        const email =event.target.email.value
+        console.log(name,email);
+        const user={name,email}
+    }
     return (
         <div>
           <h1>Please Update</h1>
+          <form onSubmit={handleUpadteUser}>
+                <input type='text' name='name' placeholder='Name'></input>
+                <br/>
+                <input  type='text' name='email' placeholder='Email' ></input>
+                <br/>
+                <input type='submit' value='Add User'></input>
+            </form>
         </div>
     );
 };
